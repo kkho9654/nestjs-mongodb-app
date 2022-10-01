452d6c5af67e6724c77561cf88729fd8c978e1a7
kkh <kkho9654@naver.com>
2022-10-01 23:27:25 +0900

    a

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -22,7 +22,8 @@ export class ConfigService {
 
     public async getMongoConfig() {
         return {
-            uri: 'mongodb+srv://' + this.get('MONGO_USER') + ':' + this.get('MONGO_PASSWORD') + '@' + this.get('MONGO_HOST') + '/' + this.get('MONGO_DATABASE'),
+            //uri: 'mongodb://admin:1234@localhost:27017/testing',
+            uri: 'mongodb://kkh3:1234@localhost:27017/testing',
             useNewUrlParser: true,
             useUnifiedTopology: true,
         };
